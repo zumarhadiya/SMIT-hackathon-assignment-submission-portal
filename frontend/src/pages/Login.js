0fcc9747c07f0fbe26bf8d59ec5848a2db66bd18
zumarhadiya <zumarhadiya2@gamail.com>
2024-08-15 21:12:43 +0500

    vercel

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -43,7 +43,7 @@ function Login() {
         }
 
         try {
-            const url = `https://smit-hackathon-assignment-submission-portal-rwpx.vercel.app//auth/login`;
+            const url = `http://localhost:8080/auth/login`;
             const response = await fetch(url, {
                 method: "POST",
                 headers: {
