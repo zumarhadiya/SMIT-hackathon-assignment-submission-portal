3294fd44f2e9e47b1dc4df559b0bf4e1414210bd
zumarhadiya <zumarhadiya2@gamail.com>
2024-08-18 01:06:13 +0500

    ..

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path'); // Add this to serve static files
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -13,20 +12,12 @@ app.get('/ping', (req, res) => {
     res.send('PONG');
 });
 
-// Middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/auth', AuthRouter);
 
-// Serve static files from the React app (client/build)
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
-    });
-}
 
 app.listen(PORT, () => {
-    console.log(`Server is running on ${PORT}`);
-});
+    console.log(`Server is running on ${PORT}`)
+})
